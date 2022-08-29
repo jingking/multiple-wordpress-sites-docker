31baf9c7e4a024b4d294b147ba6d828487c56958
jingking <github@jingking.net>
2022-08-29 19:45:53 -0400

    initial commit

---
+++ mysql_init/databases.sql
@@ -0,0 +1,7 @@
+# create databases
+CREATE DATABASE IF NOT EXISTS `wordpress_db1`;
+CREATE DATABASE IF NOT EXISTS `wordpress_db2`;
+
+# grant rights
+#GRANT ALL PRIVILEGES ON wordpress_db1.* TO 'wordpress_db_user'@'localhost';
+#GRANT ALL PRIVILEGES ON wordpress_db2.* TO 'wordpress_db_user'@'localhost';
